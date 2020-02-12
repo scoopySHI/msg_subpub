1b9e3526afc6b37bd8b715c932e320712304f7ea
fgn <nick.fiege@streetscooter.eu>
2020-02-12 09:42:12 +0100

    added custom message for testing and hexdump of received message

--- src/subpub_node.cpp
+++ src/subpub_node.cpp
@@ -24,20 +24,75 @@ private:
   std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::Imu>> imu_pub_;
   std::shared_ptr<rclcpp::Subscription<sensor_msgs::msg::Imu>> imu_sub_;
 
+
+
+void hexDump (const void * data, const int len)
+{
+    int i;
+    char buff[17];
+    const unsigned char * pc = (const unsigned char *) data;
+    // Length checks
+    if (len == 0) {
+        printf("  ZERO LENGTH\n"); return;
+    }
+    else if (len < 0) {
+        printf("  NEGATIVE LENGTH: %d\n", len); return;
+    }
+
+    // Process every byte in the data
+    for (i = 0; i < len; i++) 
+    {
+        // Multiple of 16 means new line (with line offset).
+        if ((i % 16) == 0) 
+        {
+            // Don't print ASCII buffer for the "zeroth" line.
+            if (i != 0)
+                printf ("  %s\n", buff);
+            // Output the offset.
+            printf ("  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+        printf (" %02x", pc[i]);
+
+        // And buffer a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) // isprint() may be better.
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+
+    // Pad out last line if not exactly 16 characters.
+    while ((i % 16) != 0) {
+        printf ("   ");
+        i++;
+    }
+
+    // And print the final ASCII buffer.
+    printf ("  %s\n\n", buff);
+}
+
 void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg)
 {
-  modified_msg_ = *msg;
-  //modified_msg_.angular_velocity_covariance[0] = 0.0004;
-  //modified_msg_.angular_velocity_covariance[4] = 0.0004;
-  //modified_msg_.angular_velocity_covariance[8] = 0.0004;
-  //modified_msg_.linear_acceleration_covariance[0] = 0.0004;
-  //modified_msg_.linear_acceleration_covariance[4] = 0.0004;
-  //modified_msg_.linear_acceleration_covariance[8] = 0.0004;
 
-  modified_msg_.header.stamp = this->get_clock()->now();
+    sensor_msgs::msg::Imu* dataPtr = msg.get();
 
-  imu_pub_->publish(modified_msg_);
-}
+    hexDump(dataPtr, sizeof(sensor_msgs::msg::Imu));
+
+
+    modified_msg_ = *msg;
+    //modified_msg_.angular_velocity_covariance[0] = 0.0004;
+    //modified_msg_.angular_velocity_covariance[4] = 0.0004;
+    //modified_msg_.angular_velocity_covariance[8] = 0.0004;
+    //modified_msg_.linear_acceleration_covariance[0] = 0.0004;
+    //modified_msg_.linear_acceleration_covariance[4] = 0.0004;
+    //modified_msg_.linear_acceleration_covariance[8] = 0.0004;
+
+    modified_msg_.header.stamp = this->get_clock()->now();
+
+    imu_pub_->publish(modified_msg_);
+    }
 };
 
 int main(int argc, char ** argv)
